61f8c235d8f992997ddb24992323d3c85c5d91ef
Charles Ekube <60458135+charles-ekube@users.noreply.github.com>
2020-06-09 10:36:22 +0100

    Add files via upload

---
+++ mobile-v/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+
+
+    $('.carousel').carousel({
+        shift: 30,
+        dist: 160,
+    });
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+
+
+});
